666f2d138d4d2ddfd68a77ee00b25c2e3b78afe9
Siddharth Asthana <siddharthasthana31@gmail.com>
2021-12-18 22:48:07 +0530

    Wrote function for the Youtube API in views.py
    - Implemented Pagination

---
+++ zerver/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from zerver.views import get_videos
+
+urlpatterns = [
+    path('', get_videos)
+]
--- zerver/views.py
+++ zerver/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.core import paginator
+from django.core.paginator import Paginator, EmptyPage
+from django.forms import model_to_dict
+from django.http import JsonResponse
+from zerver.models import SearchResults
 
-# Create your views here.
+
+def get_videos(request):
+    videos = SearchResults.objects.all().order_by('-published_datetime')
+    paginator = Paginator(videos, 50)
+    page_no = int(request.GET.get('page') or '1')
+    try:
+        paginated_videos = paginator.page(page_no)
+    except EmptyPage:
+        return JsonResponse({"details": "Page not found"}, status=404)
+    next_page = paginated_videos.next_page_number() \
+        if paginated_videos.has_next() else None
+    prev_page = paginated_videos.previous_page_number() \
+        if paginated_videos.has_previous() else None
+    return JsonResponse({
+        "next_page": next_page,
+        "previous_page": prev_page,
+        "videos": [model_to_dict(v) for v in list(paginated_videos)]
+    })
