a6795ce3cd492bb73f4cf03c9ab62c4669481895
Siddharth Asthana <siddharthasthana31@gmail.com>
2021-12-19 16:16:50 +0530

    Added `lookup_runserver` management command.
    Sync with lookup to start fetching API when starting `docker-compose up`

---
+++ zerver/management/commands/lookup_runserver.py
@@ -0,0 +1,21 @@
+import os
+import asyncio
+
+from django.core.management import call_command
+from django.core.management.commands.runserver import Command as BaseCommand
+
+loop = asyncio.get_event_loop()
+
+
+def start_sync():
+    print("Starting Sync with API...")
+    call_command('lookup')
+
+
+class Command(BaseCommand):
+    """Command to run runserver and sync with API."""
+
+    def handle(self, *args, **options):
+        if os.environ.get('RUN_MAIN') != 'true':
+            loop.run_in_executor(None, start_sync)
+        super(Command, self).handle(*args, **options)
